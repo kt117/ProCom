e4e9d15d3087752ec490a637901bd04e4a409f7b
kt117 <k.tohyama17@gmail.com>
2020-07-10 18:37:08 +0900

    init: other sol

---
+++ codeforces/educational/53/d_alt.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+const ll MOD = 1e9+7;
+const ll INF = 1e18;
+#define rep(i,m,n) for(ll i = (m); i <= (n); i++)
+#define zep(i,m,n) for(ll i = (m); i < (n); i++)
+#define rrep(i,m,n) for(ll i = (m); i >= (n); i--)
+#define print(x) cout << (x) << endl;
+#define printa(x,m,n) for(int i = (m); i <= n; i++){cout << (x[i]) << " ";} cout<<endl;
+
+int main(){
+    cin.tie(0); ios::sync_with_stdio(false);
+    
+    ll n, T; cin >> n >> T;
+    ll a[n]; zep(i, 0, n)cin >> a[i];
+
+    ll ans = 0;
+    zep(i, 0, n){
+        ll sm = 0, cnt = 0;
+        zep(i, 0, n){
+            if(sm + a[i] <= T){
+                sm += a[i];
+                cnt++;
+            }
+        }
+
+        if(cnt == 0)break;
+
+        ans += (T / sm) * cnt;
+        T %= sm;
+    }
+
+    print(ans)
+    
+    return 0;
+}
