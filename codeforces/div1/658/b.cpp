d6a17e377be83a8bad0c830ae02c48d86f732585
kt117 <k.tohyama17@gmail.com>
2020-07-22 04:00:33 +0900

    init: after

--- codeforces/div1/658/b.cpp
+++ codeforces/div1/658/b.cpp
@@ -18,27 +18,34 @@ int main(){
         ll n; cin >> n;
         ll p[2 * n]; zep(i, 0, 2 * n)cin >> p[i];
 
-        vector<ll> a, b;
-        a.push_back(INF + 1);
-        b.push_back(INF);
+        vector<vector<ll>> v;
+        vector<ll> cur;
+        zep(i, 0, 2 * n){
+            if(cur.size() > 0 && p[i] > cur[0]){
+                v.push_back(cur);
+                vector<ll> tmp;
+                cur.swap(tmp);
+            }
 
-        bool ok = true;
-        rrep(i, 2 * n - 1, 0){
-            if(p[i] < a.back() && p[i] < b.back()){
-                if(a.back() < b.back()){
-                    a.push_back(p[i]);
-                }else{
-                    b.push_back(p[i]);
+            cur.push_back(p[i]);
+        }
+        v.push_back(cur);
+
+        ll m = v.size();
+        ll dp[m + 1][n + 1]; memset(dp, -1, sizeof(dp));
+        dp[0][0] = 0;
+        zep(i, 0, m){
+            zep(j, 0, n){
+                if(dp[i][j] >= 0){
+                    dp[i + 1][j] = dp[i][j];
+                    if(j + v[i].size() <= n){
+                        dp[i + 1][j + v[i].size()] = i;
+                    }
                 }
-            }else if(p[i] < a.back() && p[i] > b.back()){
-                a.push_back(p[i]);
-            }else if(p[i] > a.back() && p[i] < b.back()){
-                b.push_back(p[i]);
-            }else{
-                ok = false;
-                break;
             }
         }
+
+        print((dp[m][n] >= 0)? "YES" : "NO")
     }
     
     return 0;
