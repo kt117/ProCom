cb844c48f2f556155b5f590561986d3761e1a8fa
kt117 <k.tohyama17@gmail.com>
2020-07-22 12:31:39 +0900

    init: virtual

---
+++ codeforces/educational/50/d.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+const ll MOD = 1e9+7;
+const ll INF = 1e18;
+#define rep(i,m,n) for(ll i = (m); i <= (n); i++)
+#define zep(i,m,n) for(ll i = (m); i < (n); i++)
+#define rrep(i,m,n) for(ll i = (m); i >= (n); i--)
+#define print(x) cout << (x) << endl;
+#define printa(x,m,n) for(int i = (m); i <= n; i++){cout << (x[i]) << " ";} cout<<endl;
+
+int main(){
+    cin.tie(0); ios::sync_with_stdio(false);
+    
+    ll n; cin >> n;
+    ll a[n]; zep(i, 0, n)cin >> a[i];
+    ll m; cin >> m;
+    ll b[m]; zep(i, 0, m)cin >> b[i];
+
+    ll ans = 0;
+    ll sa = 0, sb = 0;
+    ll ia = 0, ib = 0;
+    while(ia < n || ib < m){
+        if(sa <= sb){
+            if(ia == n){print(-1) return 0;}
+            sa += a[ia]; ia++;
+        }else{
+            if(ib == m){print(-1) return 0;}
+            sb += b[ib]; ib++;
+        }
+
+        if(sa == sb){
+            ans++;
+            sa = 0, sb = 0;
+        }
+    }
+
+    print((sa + sb == 0)? ans : -1)
+    
+    return 0;
+}
