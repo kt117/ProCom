6fbda3d02ec7b09f7ca0d2a16799b9ca9379971a
kt117 <k.tohyama17@gmail.com>
2020-11-22 13:47:43 +0900

    init: nsy

---
+++ atcoder/abc/180/f.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+const ll MOD = 1e9+7;
+const ll INF = 1e18;
+#define rep(i,m,n) for(ll i = (m); i <= (n); i++)
+#define zep(i,m,n) for(ll i = (m); i < (n); i++)
+#define rrep(i,m,n) for(ll i = (m); i >= (n); i--)
+#define print(x) cout << (x) << endl;
+#define printa(x,m,n) for(int i = (m); i <= n; i++){cout << (x[i]) << " ";} cout<<endl;
+
+const long long MAX = 2e5+100;
+long long fac[MAX], finv[MAX], inv[MAX];
+
+// テーブルを作る前処理
+void COMinit() {
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++){
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+    }
+}
+
+// 二項係数計算
+long long COM(int n, int k){
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
+ll n, m, l, dp[310][310];
+
+int main(){
+    cin.tie(0); ios::sync_with_stdio(false);
+    COMinit();
+
+    cin >> n >> m >> l;
+
+    dp[0][0] = 1;
+
+    rrep(dn, l, 1){
+        if(dn == l){
+            rep(i, 0, n - k * dn){
+                ll tmp = 1;
+                rep(k, 1, n / dn){
+                    
+                    rep(j, 0, m - dn){
+                        
+                    }
+                }
+            }
+        }
+    }
+
+    print(dp[0][0])
+
+    return 0;
+}
