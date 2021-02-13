a3e1f3f4198d064ed0dd1ca2cd61311db58a5933
kt117 <k.tohyama17@gmail.com>
2021-02-13 20:35:20 +0900

    init: after

--- atcoder/abc/191/f.cpp
+++ atcoder/abc/191/f.cpp
@@ -10,10 +10,41 @@ const ll INF = 1e18;
 #define print(x) cout << (x) << endl;
 #define printa(x,m,n) for(int i = (m); i <= n; i++){cout << (x[i]) << " ";} cout<<endl;
 
+long long gcd(long long a, long long b){return (b == 0)? a : gcd(b, a % b);}
+
 int main(){
     cin.tie(0); ios::sync_with_stdio(false);
-    
-    
+  
+    ll n; cin >> n;
+    ll a[n]; zep(i, 0, n)cin >> a[i];
+
+    ll mn = a[0]; zep(i, 0, n)mn = min(mn, a[i]);
+
+    map<ll, ll> s;
+    zep(i, 0, n){
+        for(ll j = 1; j * j <= a[i]; j++){
+            if(a[i] % j == 0){
+                if(s.find(j) == s.end()){
+                    s[j] = a[i];
+                }else{
+                    s[j] = gcd(s[j], a[i]);
+                }
+
+                ll k = a[i] / j;
+                if(s.find(k) == s.end()){
+                    s[k] = a[i];
+                }else{
+                    s[k] = gcd(s[k], a[i]);
+                }
+            }
+        }
+    }
+
+    ll ans  = 0;
+    for(auto it = s.begin(); it != s.end(); it++){
+        if((*it).first <= mn && (*it).first == (*it).second)ans++;
+    } 
+    print(ans)
     
     return 0;
 }
