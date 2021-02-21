efbd4017947a742a8ea555fbe17b13b2d7950dce
kt117 <k.tohyama17@gmail.com>
2021-02-21 20:54:54 +0900

    init: after

--- atcoder/abc/191/d.cpp
+++ atcoder/abc/191/d.cpp
@@ -10,26 +10,32 @@ const ll INF = 1e18;
 #define print(x) cout << (x) << endl;
 #define printa(x,m,n) for(int i = (m); i <= n; i++){cout << (x[i]) << " ";} cout<<endl;
 
+ll M = 10000;
+
 int main(){
     cin.tie(0); ios::sync_with_stdio(false);
     
     double x, y, r; cin >> x >> y >> r;
 
+    ll X = round(x * M);
+    ll Y = round(y * M);
+    ll R = round(r * M);
+
     ll ans = 0;
-    rep(i, floor(x - r), ceil(x + r)){
-        ll ok = ceil(y);
-        ll ng = ceil(y + r);
-        
+    rep(i, (X - R) / M - 10, (X + R) / M + 10){
+        ll xx = (i * M - X) * (i * M - X);
+        ll dy2 = R * R - xx;
 
-        while(ng - ok > 1){
-            double mid = (ok + ng) / 2;
-            if(f(mid)){
-                ok = mid;
-            }else{
+        if(dy2 >= 0){
+            ll dy = round(sqrt(dy2));
 
-            }
+            ll u = (Y + dy) / M + 10; zep(i, 0, 20){if((u * M - Y) * (u * M - Y) + xx <= R * R)break; u--;}
+            ll l = (Y - dy) / M - 10; zep(i, 0, 20){if((l * M - Y) * (l * M - Y) + xx <= R * R)break; l++;}
+            //cout << i << u << l << endl;
+            if(u >= l)ans += u - l + 1;
         }
     }
+    print(ans)
     
     return 0;
 }
