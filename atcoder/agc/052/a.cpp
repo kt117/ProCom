d91e4ca595753eaa0570ab35e62d6c497eb2cd54
kt117 <k.tohyama17@gmail.com>
2021-03-08 09:14:56 +0900

    init: after

---
+++ atcoder/agc/052/a.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+const ll MOD = 1e9+7;
+const ll INF = 1e18;
+#define rep(i,m,n) for(ll i = (m); i <= (n); i++)
+#define zep(i,m,n) for(ll i = (m); i < (n); i++)
+#define rrep(i,m,n) for(ll i = (m); i >= (n); i--)
+#define print(x) cout << (x) << endl;
+#define printa(x,m,n) for(int i = (m); i <= n; i++){cout << (x[i]) << " ";} cout<<endl;
+
+int main(){
+    cin.tie(0); ios::sync_with_stdio(false);
+    
+    ll t; cin >> t;
+    while(t--){
+        ll n; cin >> n;
+        string s[3]; zep(i, 0, 3)cin >> s[i];
+
+        zep(i, 0, n)cout << 0;
+        zep(i, 0, n)cout << 1;
+        cout << 0 << endl;
+    }
+    
+    return 0;
+}
