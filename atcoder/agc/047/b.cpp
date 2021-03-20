2a450c54eed84c2b9c15578d73d8918cf68c68a6
kt117 <k.tohyama17@gmail.com>
2021-03-21 08:54:32 +0900

    init: wip

---
+++ atcoder/agc/047/b.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+const ll MOD = 1e9+7;
+const ll INF = 1e18;
+#define rep(i,m,n) for(ll i = (m); i <= (n); i++)
+#define zep(i,m,n) for(ll i = (m); i < (n); i++)
+#define rrep(i,m,n) for(ll i = (m); i >= (n); i--)
+#define print(x) cout << (x) << endl;
+#define printa(x,m,n) for(int i = (m); i <= n; i++){cout << (x[i]) << " ";} cout<<endl;
+
+int main(){
+    cin.tie(0); ios::sync_with_stdio(false);
+    
+    ll n; cin >> n;
+    string s[n]; zep(i, 0, n)cin >> s[i]; 
+
+    zep(i, 0, n)reverse(s[i].begin(), s[i].end());
+    
+    map<string, int> mp;
+    zep(i, 0, n){
+        string t = s[i];
+        int x = t.back() - 'a';
+        t.pop_back();
+        mp[t] |= (1 << x);
+    }
+
+    int ans = 0;
+    zep(i, 0, n){
+        string t = s[i];
+        int bit = 0;
+
+        while(t.size()){
+            bit |= (1 << (t.back() - 'a'));
+            t.pop_back();
+
+            if(mp.find(t) != mp.end()){
+                ans += __builtin_popcount(bit & mp[t]);
+            }
+        }
+    }
+    
+    print(ans - n);
+    
+    return 0;
+}
