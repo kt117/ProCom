d401f3705fcb55992c4d3a64979ad1a0cf26f0ab
kt117 <k.tohyama17@gmail.com>
2021-04-11 04:07:24 +0900

    init: practice

---
+++ atcoder/agc/049/b.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+const ll MOD = 1e9+7;
+const ll INF = 1e18;
+#define rep(i,m,n) for(ll i = (m); i <= (n); i++)
+#define zep(i,m,n) for(ll i = (m); i < (n); i++)
+#define rrep(i,m,n) for(ll i = (m); i >= (n); i--)
+#define print(x) cout << (x) << endl;
+#define printa(x,m,n) for(int i = (m); i <= n; i++){cout << (x[i]) << " ";} cout<<endl;
+
+int main(){
+    cin.tie(0); ios::sync_with_stdio(false);
+    
+    ll n; cin >> n;
+    string s, t; cin >> s >> t;
+
+    ll ans = 0;
+    queue<ll> p, q;
+    zep(i, 0, n){
+        if(t[i] == '1')q.push(i);
+        if(s[i] == '1'){
+            if(!p.empty()){
+                ans += i - p.front();
+                p.pop();
+            }else if(!q.empty()){
+                ans += i - q.front();
+                q.pop();
+            }else{
+                p.push(i);
+            }
+        }
+    }
+
+    if(p.empty() && q.empty()){
+        print(ans)
+    }else{
+        print(-1)
+    }
+    
+    return 0;
+}
