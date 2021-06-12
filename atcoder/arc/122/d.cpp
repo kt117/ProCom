31582669f70af92560038218466475755bb04276
kt117 <k.tohyama17@gmail.com>
2021-06-13 01:49:21 +0900

    init: after

--- atcoder/arc/122/d.cpp
+++ atcoder/arc/122/d.cpp
@@ -10,52 +10,37 @@ const ll INF = 1e18;
 #define print(x) cout << (x) << endl;
 #define printa(x,m,n) for(int i = (m); i <= n; i++){cout << (x[i]) << " ";} cout<<endl;
 
-ll n, ans, cnt, p[200020][30];
-vector<ll> a;
 
-void f(vector<ll> v, ll d, ll pp){
-    if(d < 0 || v.size() == 0)return;
+ll g(ll bit, vector<ll> u, vector<ll> v, ll d){
+    if(u.size() == 0 || v.size() == 0)return INF;
+    if(d < 0)return bit;
+
+    vector<ll> uu[2], vv[2];
     
-    printa(v, 0, v.size() - 1)
-    vector<ll> u[2];
-    
-    zep(i, 0, v.size()){
-        u[v[i] >> d].push_back(v[i]);
-    }
-    zep(i, 0, u[0].size())cout << u[0][i] << " "; cout << endl;
-    zep(i, 0, u[1].size())cout << u[1][i] << " "; cout << endl;
-    print("------")
-    if(pp != -1){
-        if(u[0].size() % 2 == 1){
-            if(p[pp][d] == -1){
-                p[pp][d] = 0;
-            }else if(p[pp][d] == 1){
-                ans += (1LL << d);
-            }
+    zep(i, 0, u.size())uu[(u[i] >> d) & 1].push_back(u[i]);
+    zep(i, 0, v.size())vv[(v[i] >> d) & 1].push_back(v[i]);
 
-            f(u[0], d - 1, pp);
-            f(u[1], d - 1, -1);
-        }else{
-            if(p[pp][d] == -1){
-                p[pp][d] = 1;
-            }else if(p[pp][d] == 0){
-                ans += (1LL << d);
-            }
-
-            f(u[0], d - 1, -1);
-            f(u[1], d - 1, pp);
-        }
+    if((uu[0].size() > 0 && vv[0].size() > 0) || (uu[1].size() > 0 && vv[1].size() > 0)){
+        return min(g(bit, uu[0], vv[0], d - 1), g(bit, uu[1], vv[1], d - 1));
     }else{
-        if(u[0].size() % 2 == 1){
-            pp = cnt; cnt++;
-            ans += (1LL << d);
+        return min(
+            g(bit | (1LL << d), uu[0], vv[1], d - 1), 
+            g(bit | (1LL << d), uu[1], vv[0], d - 1)
+        );
+    }
+}
 
-            f(u[0], d - 1, pp);
-            f(u[1], d - 1, pp);
-        }else{
-            f(u[0], d - 1, -1);
-            f(u[1], d - 1, -1);
-        }
+
+ll f(vector<ll> v, ll d){
+    if(v.size() == 0 || d < 0)return 0;
+
+    vector<ll> u[2];
+    zep(i, 0, v.size())u[(v[i] >> d) & 1].push_back(v[i]);
+
+    if(u[0].size() % 2 == 0){
+        return max(f(u[0], d - 1), f(u[1], d - 1));
+    }else{
+        return g((1LL << d), u[0], u[1], d - 1);
     }
 }
 
@@ -63,16 +48,12 @@ void f(vector<ll> v, ll d, ll pp){
 int main(){
     cin.tie(0); ios::sync_with_stdio(false);
     
-    cin >> n; n *= 2;
+    ll n; cin >> n; n *= 2;
     ll a[n]; zep(i, 0, n)cin >> a[i];
     vector<ll> v;
     zep(i, 0, n)v.push_back(a[i]);
 
-    memset(p, -1, sizeof(p));
-    
-    // f(v, 30, -1);
-    f(v, 2, -1);
-    print(ans)
+    print(f(v, 30));
 
     return 0;
 }
