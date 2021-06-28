e5997d11bb093c23843bde3485737a10085008dd
kt117 <k.tohyama17@gmail.com>
2021-06-28 17:41:37 +0900

    init: after

--- atcoder/agc/054/c.cpp
+++ atcoder/agc/054/c.cpp
@@ -10,31 +10,74 @@ const ll INF = 1e18;
 #define print(x) cout << (x) << endl;
 #define printa(x,m,n) for(int i = (m); i <= n; i++){cout << (x[i]) << " ";} cout<<endl;
 
+template<typename T> class segtree {
+private:
+    int n,sz,h;
+    vector<T> node, lazy;
+    void eval(int k) {
+        if(lazy[k]){
+            node[k] += lazy[k];
+            if(k < n) {
+                lazy[k*2] += lazy[k] / 2, lazy[k*2+1] += lazy[k] / 2;
+            }
+            lazy[k] = 0;
+        }
+    }
+
+public:
+    segtree(const vector<T>& v) : n(1), sz((int)v.size()), h(0) {
+        while(n < sz) n *= 2, h++;
+        node.resize(2*n, 0), lazy.resize(2*n, 0);
+        for(int i = 0; i < sz; i++) node[i+n] = v[i];
+        for(int i = n-1; i >= 1; i--) node[i] = node[2*i] + node[2*i+1];
+    }
+    void range(int a, int b, T x, int k=1, int l=0, int r=-1){
+        if(r < 0) r = n;
+        eval(k);
+        if(b <= l || r <= a){
+            return;
+        }
+        if(a <= l && r <= b){
+            lazy[k] += (r-l)*x;
+            eval(k);
+        }else{
+            range(a, b, x, 2*k, l, (l+r)/2);
+            range(a, b, x, 2*k+1, (l+r)/2, r);
+            node[k] = node[2*k] + node[2*k+1];
+        }
+    }
+    T query(int a, int b) {
+        a += n, b += n - 1;
+        for(int i = h; i > 0; i--) eval(a >> i), eval(b >> i);
+        b++;
+        T res1 = 0, res2 = 0;
+        while(a < b) {
+            if(a & 1) eval(a), res1 += node[a++];
+            if(b & 1) eval(--b), res2 += node[b];
+            a >>= 1, b >>= 1;
+        }
+        return res1 + res2;
+    }
+    // void print(){for(int i = 0; i < sz; i++) cout<<query(i,i+1)<< " ";cout<<endl;}
+};
+
 int main(){
     cin.tie(0); ios::sync_with_stdio(false);
     
     ll n, k; cin >> n >> k;
     ll p[n]; zep(i, 0, n)cin >> p[i];
+    ll ip[n + 1]; zep(i, 0, n)ip[p[i]] = i;
 
-    ll ten[n]; memset(ten, 0, sizeof(ten));
-    zep(i, 0, n){zep(j, 0, i)if(p[j] > p[i])ten[i]++;
+    vector<ll> v(n, 0);
+    segtree<ll> sg(v);
 
     ll ans = 1;
-    rep(j, 1, n){
-        ll at = -1;
-        zep(i, 0, n)if(p[i] == j){at = i; break;}
-
-        ll ten = 0;
-        zep(i, 0, at)if(p[i] > j)ten++;
-        
-        if(ten == k){
-            ll cnt = 0;
-            while(at > 0 && p[at - 1] < j){cnt++; at--;}
-            
-            ans *= cnt + 1;
+    rrep(i, n, 1){
+        if(sg.query(0, ip[i]) == k){
+            ans *= sg.query(ip[i] + 1, n) + 1;
             ans %= MOD;
         }
-        // cout << at << " " << ten << " " << ans << endl;
+        sg.range(ip[i], ip[i] + 1, 1);
     }
     print(ans)
     
